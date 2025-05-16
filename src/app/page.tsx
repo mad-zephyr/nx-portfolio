6f8aa083fd482865be8d0f05e3ca37d9dd638e24
Vasilii Iatiuc <Vasilii.Iatiuc@endava.com>
2025-05-17 01:22:09 +0300

    add netlify toml

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,11 +25,11 @@ export default function Home() {
         height: "100vh",
         cursor: "grab",
       }}
-      camera={{ position: [0, 0, 10], fov: 70 }}
+      camera={{ position: [0, 0, 12], fov: 70 }}
     >
       <InfiniteImageGrid textureUrls={imagePaths} gridSize={9} spacing={5} />
       <EffectComposer multisampling={3} enableNormalPass={true}>
-        <AntiFisheye strength={0.1} />
+        <AntiFisheye strength={0.05} />
         <Noise opacity={0.025} />
         <Vignette eskil={false} offset={0.15} darkness={0.5} />
       </EffectComposer>
